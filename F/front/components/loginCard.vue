49fed8aa435916626f888988368a3021752853a5
kkm4512 <nayoun440@naver.com>
2024-02-13 23:40:09 +0900

    1
    1

--- F/front/components/loginCard.vue
+++ F/front/components/loginCard.vue
@@ -1,100 +1,49 @@
 <template>
 
     <div class="flex justify-center items-center h-screen">
-        <div class="block max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 p-12">
-            <input 
-            type="text" 
-            class="block p-2.5 
-            w-full 
-            text-sm 
-            rounded-lg 
-            border 
-            border-gray-300 
-            focus:ring-blue-500 
-            focus:border-blue-500 
-            text-gray-600 
-            dark:border-gray-600 
-            dark:placeholder-gray-400 
-            bg-white 
-            dark:focus:ring-blue-500 
-            dark:focus:border-blue-500 
-            mt-2
-            "
-            placeholder="Email">
-
-            <input 
-            type="password" 
-            class="block p-2.5 
-            w-full 
-            text-sm 
-            rounded-lg 
-            border 
-            border-gray-300 
-            focus:ring-blue-500 
-            focus:border-blue-500 
-            text-gray-600 
-            dark:border-gray-600 
-            dark:placeholder-gray-400 
-            bg-white 
-            dark:focus:ring-blue-500 
-            dark:focus:border-blue-500 
-            mt-2
-            "
-            placeholder="Password">            
-
-            <input 
-            type="text" 
-            class="block p-2.5 
-            w-full 
-            text-sm 
-            rounded-lg 
-            border 
-            border-gray-300 
-            focus:ring-blue-500 
-            focus:border-blue-500 
-            text-gray-600 
-            dark:border-gray-600 
-            dark:placeholder-gray-400 
-            bg-white 
-            dark:focus:ring-blue-500 
-            dark:focus:border-blue-500 
-            mt-2
-            "
-            placeholder="NickName">            
-
-            <button type="button" 
+        <div class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 w-3/12 p-12">
+            <h1 class="text-white flex justify-center text-3xl"> Login </h1>
+            <div class="mt-8">
+                <label for="Email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nayoun440@naver.com" required>
+            </div>
+            <div class="mt-8">
+                <label for="Password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input type="password" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="*********" required>
+            </div>
+            <div class="mt-8">
+                <label for="Nickname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nickname</label>
+                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required>
+            </div>               
+            <div class="flex justify-between p-2">
+                <NuxtLink to="/passwordFind">
+                    <div class="text-white cursor-pointer hover:text-blue-300">
+                        비밀번호찾기
+                    </div>
+                </NuxtLink>
+                <NuxtLink to="/signUp">
+                    <div class="text-white cursor-pointer hover:text-blue-300">
+                        회원가입
+                    </div>
+                </NuxtLink>
+            </div>
+            <div
             class="text-white 
-            mt-5
-            bg-blue-700 
-            hover:bg-blue-800 
             focus:ring-4 
+            focus:outline-none 
             focus:ring-blue-300 
-            font-medium rounded-lg 
-            text-sm px-5 
+            font-medium 
+            rounded-lg 
+            text-sm w-full 
+            sm:w-auto 
+            px-5 
             py-2.5 
-            me-2 
-            mb-2 
+            text-center 
             dark:bg-blue-600 
             dark:hover:bg-blue-700 
-            focus:outline-none 
-            dark:focus:ring-blue-800">로그인</button>
-
-            <button type="button" 
-            class="text-white 
-            mt-5
-            bg-blue-700 
-            hover:bg-blue-800 
-            focus:ring-4 
-            focus:ring-blue-300 
-            font-medium rounded-lg 
-            text-sm px-5 
-            py-2.5 
-            me-2 
-            mb-2 
-            dark:bg-blue-600 
-            dark:hover:bg-blue-700 
-            focus:outline-none 
-            dark:focus:ring-blue-800">회원가입</button>            
+            dark:focus:ring-blue-800
+            mt-5"
+            >로그인</div>         
         </div>
         
      </div>
